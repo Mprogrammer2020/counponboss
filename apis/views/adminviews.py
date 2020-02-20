4343d52775845b6101f4eeb7428ca5796f739d45
diksha09 <diksha455sharma.com>
2020-02-20 13:22:35 +0530

    sdsd

--- apis/views/adminviews.py
+++ apis/views/adminviews.py
@@ -772,7 +772,7 @@ def Add_Country(request):
                 return Response({"message" : errorMessageUnauthorised, "status" : "0"}, status=status.HTTP_401_UNAUTHORIZED)
 
             country_exist = Country.objects.filter(name = request.data['countryName'])
-            if country_exist is not None:
+            if country_exist is None:
 
                 country_detail=Country.objects.create(name = request.data['countryName'],
                                                         latitude = request.data['lat'],
